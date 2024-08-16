212cd387f3a10a58416bf759859ad4dc374acbd6
Zhizhen He <hezhizhen.yi@gmail.com>
2024-08-16 21:26:35 +0800

    update neovim

--- .config/nvim/lua/plugins/default.lua
+++ .config/nvim/lua/plugins/default.lua
@@ -96,4 +96,12 @@ return {
       require("log-highlight").setup({})
     end,
   },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }
